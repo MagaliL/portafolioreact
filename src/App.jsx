95b0e20cd35578004ba01457027979963e178f6e
Magali Espinosa <magalizethe@gmail.com>
2025-09-30 20:06:21 -0600

    Se hizo el frente, con el título, el navbar, texto y la imagen principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import Header from './components/Header.jsx';
+import portada from '../src/assets/imagen.png';
 
 
 function App() {
@@ -8,7 +9,17 @@ function App() {
     <>
       <Header />
       <main>
-        <p>Hola</p>
+        <div className='frente'>
+          <section className='saludo'>
+              <h1 className='titulo'>HI I'M MAGALI, </h1>
+              <h2 className='subtitulo'>Software Engenieer</h2>
+              <p>Building modern, responsive, and user-centric web experiences. Data analysis to help drive decision-making</p>
+          </section>
+          <aside className='image'>
+            <img src={portada} alt="Imagen de una computadora de la que salen figuras geométricas de un color cobre brillante" />
+            {/* Tal vez después se pueda hacer una función de girar  */}
+          </aside>
+        </div>
       </main>
     </>
   );
