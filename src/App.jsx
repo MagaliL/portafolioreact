dc65c48b1375b2adbdc4d024f3310e25332b3bf0
Magali Espinosa <magalizethe@gmail.com>
2025-09-30 18:39:23 -0600

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from './components/Header.jsx';
+
+
+function App() {
+
+  return(
+    <>
+      <Header />
+      <main>
+        <p>Hola</p>
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './Header.module.css';
+
+function Header() {
+    return(
+
+    < header className={styles.header}>
+        <nav className={styles.header__nav}>
+           <ul className={styles.header__nav__list}>
+            <li className={styles.header__nav__list__element}>About</li>
+           <li className={styles.header__nav__list__element}>Proyects</li>
+           <li className={styles.header__nav__list__element}>Contact</li>
+           </ul>
+        </nav>
+    </header>
+    );
+}
+
+export default Header; 
