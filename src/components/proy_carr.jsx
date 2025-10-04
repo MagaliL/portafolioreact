e4bb054739af99c3397dae713bda89c7d10f9c8c
Magali Espinosa <magalizethe@gmail.com>
2025-10-04 11:10:23 -0600

    Corrige nombre de componente para Netlify

--- src/components/proy_carr.jsx
+++ src/components/proy_carr.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from './Proy_carr.module.css';
-import Button from './Button.jsx';
+import Button from './ButtonTemp.jsx.jsx';
 
 function Proy_carr ({project}){
 
